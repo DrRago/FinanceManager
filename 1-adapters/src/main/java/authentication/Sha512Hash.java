c379833a0e2b262250a1bb48fd7ecde1b0533da1
Leonhard Gahr <leonhard.gahr@gmail.com>
2021-05-29 17:42:33 +0200

    fix issue in hash generation

--- 1-adapters/src/main/java/authentication/Sha512Hash.java
+++ 1-adapters/src/main/java/authentication/Sha512Hash.java
@@ -3,22 +3,20 @@ package authentication;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Sha512Hash {
     public static String hash(String text) {
+        String toReturn = null;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] messageDigest = md.digest(text.getBytes());
-            BigInteger no = new BigInteger(1, messageDigest);
-            StringBuilder hash = new StringBuilder(no.toString(16));
-            while (hash.length() < 32) {
-                hash.insert(0, "0");
-            }
-            return hash.toString();
-        } catch (NoSuchAlgorithmException e) {
+            MessageDigest digest = MessageDigest.getInstance("SHA-512");
+            digest.reset();
+            digest.update(text.getBytes(StandardCharsets.UTF_8));
+            toReturn = String.format("%0128x", new BigInteger(1, digest.digest()));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return "";
+
+        return toReturn;
+
     }
 }
